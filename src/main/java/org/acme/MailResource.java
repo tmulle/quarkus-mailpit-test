4ad78f3f3d3dfc867eae7c706a21dcfe68854389
tmulle <tallies.hoofed-0w@icloud.com>
2023-10-14 09:36:40 -0400

    Adding latest mailpit extension and adding reactive endpoint

--- src/main/java/org/acme/MailResource.java
+++ src/main/java/org/acme/MailResource.java
@@ -6,11 +6,14 @@ package org.acme;
 
 import io.quarkus.mailer.Mail;
 import io.quarkus.mailer.Mailer;
+import io.quarkus.mailer.reactive.ReactiveMailer;
 import io.smallrye.common.annotation.Blocking;
+import io.smallrye.mutiny.Uni;
 import jakarta.inject.Inject;
 import jakarta.ws.rs.GET;
 import jakarta.ws.rs.Path;
 import java.util.List;
+import org.jboss.logging.Logger;
 
 /**
  *
@@ -22,21 +25,36 @@ public class MailResource {
     @Inject
     Mailer mailer;
 
+    @Inject
+    ReactiveMailer reactiveMailer;
+    
+    @Inject
+    Logger log;
+
     @GET
     @Blocking
     public void sendEmail() {
+
+        Mail m = new Mail();
+        m.setFrom("tmulle@gmail.com");
+        m.setTo(List.of("quarkus@quarkus.io"));
+        m.setText("A simple email sent from a Quarkus application.");
+        m.setSubject("Ahoy from Quarkus");
         
-//        Mail m = new Mail();
-//        m.setFrom("tmulle@gmail.com");
-//        m.setTo(List.of("quarkus@quarkus.io"));
-//        m.setText("A simple email sent from a Quarkus application.");
-//        m.setSubject("Ahoy from Quarkus");
-//        mailer.send(m);
-        mailer.send(
-                Mail.withText("quarkus@quarkus.io",
-                        "Ahoy from Quarkus",
-                        "A simple email sent from a Quarkus application."
-                )
-        );
+        log.infof("Regular Mail is about to send %s", m);
+        mailer.send(m);
+        log.infof("Done sending regular mail");
+       
+    }
+
+    @GET
+    @Path("/reactive")
+    public Uni<Void> sendEmailUsingReactiveMailer() {
+         Mail m = new Mail();
+        m.setFrom("tmulle@gmail.com");
+        m.setTo(List.of("quarkus@quarkus.io"));
+        m.setText("A simple email sent from a Quarkus application.");
+        m.setSubject("Ahoy from Quarkus");
+        return reactiveMailer.send(m).onItem().invoke(() -> log.infof("Reactive Mail is about to send %s", m));
     }
 }
